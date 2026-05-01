59e930a2ac4401d2e6536c8dec4f845d01f5e727
el_da <el_daexample.com>
2026-05-02 00:06:25 +0300

    [new]: dev - Chat: Frontend for mobile

--- assets/controllers/chat-list_controller.js
+++ assets/controllers/chat-list_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ["link"];
+    static targets = ["menu", "talk", "link"];
     static classes = ["selected"];
 
     select(event) {
@@ -13,5 +13,28 @@ export default class extends Controller {
         // 2. Добавляем класс только кликнутой ссылке
         // Используем event.currentTarget, чтобы попасть именно на <a>
         event.currentTarget.classList.add(this.selectedClass);
+
+        // 3. Логика переключения (только мобильные)
+        if (window.innerWidth < 768) {
+            this.menuTarget.classList.add("d-none"); // Прячем список
+            this.talkTarget.classList.remove("d-none"); // Показываем чат
+            this.talkTarget.classList.add("d-flex"); // Для корректной работы flex-структуры
+        }
+    }
+
+    backToList(event) {
+        event.preventDefault();
+
+        // 1. Логика переключения (только мобильные)
+        if (window.innerWidth < 768) {
+             // 2. Удаляем класс у всех ссылок в списке
+            this.linkTargets.forEach(el => {
+                el.classList.remove(this.selectedClass);
+            });
+
+            this.talkTarget.classList.add("d-none"); // Прячем чат
+            this.talkTarget.classList.remove("d-flex");
+            this.menuTarget.classList.remove("d-none"); // Показываем список
+        }
     }
 }
